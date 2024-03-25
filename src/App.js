3a6b85ab8eca650b404771144711e70dedc0b909
cobalteek <ravshankenz@gmail.com>
2024-03-25 19:53:37 +0500

    Deleted unused lines of code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, {useRef, useState} from 'react'
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import React, {useState} from 'react'
 import './styles/App.css'
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+
 function App() {
 
-    const [posts, setPosts] = useState( [
-        {id:1, title: 'Javascript', body: 'Decription'},
-        {id:2, title: 'Javascript 2', body: 'Decription'},
-        {id:3, title: 'Javascript 3', body: 'Decription'},
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Decription'},
+        {id: 2, title: 'Javascript 2', body: 'Decription'},
+        {id: 3, title: 'Javascript 3', body: 'Decription'},
     ])
 
     const createPost = (newPost) => {
@@ -23,24 +19,24 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-  return (
-    <div className="App">
-        <PostForm create={createPost}/>
-        <hr style={{margin: '15px 0'}}/>
-        <div>
-            <select>
-                <option value="value1">По названию</option>
-                <option value="value1">По описанию</option>
-            </select>
+    return (
+        <div className="App">
+            <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <select>
+                    <option value="value1">По названию</option>
+                    <option value="value1">По описанию</option>
+                </select>
+            </div>
+            {posts.length
+                ?
+                <PostList remove={removePost} posts={posts} title="Посты про JS"/>
+                :
+                <h1 style={{textAlign: "center"}}>Посты не найдены!</h1>
+            }
         </div>
-        {posts.length
-            ?
-                <PostList remove={removePost} posts={posts} title = "Посты про JS"/>
-            :
-                <h1 style={{textAlign:"center"}}>Посты не найдены!</h1>
-        }
-    </div>
-  );
+    );
 }
 
 export default App;
