cb688ea76c198931a6e9e7456c63b7f13ed67aad
cobalteek <ravshankenz@gmail.com>
2024-03-25 20:32:39 +0500

    Yet another change

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MyButton from "./UI/button/MyButton";
-import 'src/styles/PostItem.css'
+import 'src/styles/PostItem.css';
 
 const PostItem = (props) => {
     return (
